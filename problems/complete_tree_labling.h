e3bd5ee29716dd5cf6809065fc95135601a57f98
Chen HANG <nporsche@163.com>
2014-01-22 09:00:40 +0800

    update complete_tree_labling

--- problems/complete_tree_labling.h
+++ problems/complete_tree_labling.h
@@ -19,6 +19,20 @@ ostream& operator << (ostream& os, const big_integer& x)
 	//TODO:
 	return os;
 }
+
+//假设node_count(k,d) 为深度为d,叉数为k的节点数量，则：
+//node_count(k,0) = 1, 深度为0的任意叉数的节点数量为0
+//node_count(k,d) = k * node(k,d-1) + 1; 递推关系式
+//
+//我们需要找到get_labling_count的递归关系式
+//get_lable_count(k,d)总共有node_count(k,d)个数，第一个
+//数必须要放在根节点，还剩下node_count(k,d)-1个数，这些数
+//要分成k组 , P = node_count(k,d-1)分法有：
+//X = C(P*k, P) * C(P*(k-1), P)*...*C(P*1, P) = (kP)!/(P!)^k
+//
+//每组内部的排列是get_labling_count(k,d-1) 
+//所有一共的排列是get_labling_count(k,d-1)^k * X 
+
 big_integer get_labling_count(int k, int d)
 {
 	big_integer result;
