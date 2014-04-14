63f160bf72f9affe5bfe9aab4ff641b2266bdb4a
nporsche <nporsche@163.com>
2014-04-14 17:02:42 +0800

    resolve vito problem
    resolve vito problem
    PC ID: 110401

--- problems/vito.h
+++ problems/vito.h
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main()
 {
+	int set = 0;
+	cin >> set;
+	cin.ignore();
+	while (set--)
+	{
+		string line;
+		getline(cin, line);
+		istringstream iss(line);
+		int num_of_relations = 0;
+		iss >> num_of_relations;
+		vector<int> relation_addresses;
+		while (num_of_relations--)
+		{
+			int x;
+			iss >> x;
+			relation_addresses.push_back(x);
+		}
+		
+		sort(relation_addresses.begin(), relation_addresses.end());
+		int middle = relation_addresses[relation_addresses.size() / 2];
+		int sum = 0;
+		for (auto x : relation_addresses)
+		{
+			sum += abs(middle - x);
+		}
+
+		cout << sum << endl;
+	}
 	return 0;
 }
